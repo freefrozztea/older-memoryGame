385ee9b22a1de2d9ac3d1469f2bb9ea341a18fc6
Felipe Alzamora <luisfalmi@gmail.com>
2021-02-09 00:01:35 -0300

    older version memory game

---
+++ app.js
@@ -0,0 +1,124 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+
+    let cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'  
+        }
+    ]
+
+    cardArray.sort(() => 0.5 - Math.random())
+    //cardArray = _.shuffle(cardArray);
+
+    /* Math.random() NO provee números aleatorios con seguridad criptográfica.
+    No deben ser usados para algo relacionado con seguridad. En vez de eso, 
+    usar la API Web Crypto, y más precisamente 
+    el método: window.crypto.getRandomValues(). */
+
+    const grid = document.querySelector('.grid');
+    const resultDisplay = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
+
+    //create your board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    //check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        
+        if(optionOneId == optionTwoId) {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Le hiciste click a la misma imagen 😅')
+        }
+        else if (cardsChosen[0] === cardsChosen[1]) {
+            alert('Encontraste un emparejamiento')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Lo siento, intenta de nuevo')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        resultDisplay.textContent = cardsWon.length
+
+        if  (cardsWon.length === cardArray.length/2) {
+        resultDisplay.textContent = ' Felicitaciones!! Ganaste la partida'
+        }
+    }
+
+    //flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length ===2) {
+        setTimeout(checkForMatch, 500)
+        }
+    }
+
+  createBoard()
+})
